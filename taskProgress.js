cdff833c822d626cc1812b11529567a8bb8c6548
Krish Shah <kshah@trueigtech.com>
2026-01-21 14:54:13 +0530

    Created: Task Progress generator function

---
+++ taskProgress.js
@@ -0,0 +1,18 @@
+function* taskProgress(){
+    let progress = 0;
+    while(progress<=100){
+        yield "Current Progress: " + progress++ + "%";
+    }
+}
+let generator = taskProgress();
+
+const intervalId = setInterval(() => {
+    let result = generator.next();
+
+    if(result.done){
+        console.log("Task Completed");
+        clearInterval(intervalId);
+    }else{
+        console.log(result.value);
+    }
+},100); 
