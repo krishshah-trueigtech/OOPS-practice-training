bf1d6ba2118cfe80b72b54f6be9bce1c29519b59
Krish Shah <kshah@trueigtech.com>
2026-01-21 13:16:39 +0530

    Created: additional method: completeTask().

--- member.js
+++ member.js
@@ -1,4 +1,11 @@
 import User from "./user.js"
 
 class Member extends User{
+    completeTask(){
+        console.log("Completed Task");
+    }
 }
+
+const newMember = new Member("Krish","Software Engineer","");
+newMember.completeTask();
+newMember.login();
