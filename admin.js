1a6feb325d1014dfb528b679da26d889222e1192
Krish Shah <kshah@trueigtech.com>
2026-01-21 12:46:49 +0530

    Created: Method createTask()

--- admin.js
+++ admin.js
@@ -1,4 +1,11 @@
 import User from "./user.js"
 
 class Admin extends User{
+    createTask(){
+        console.log("created Task")
+    }
 }
+
+const ad = new Admin("Krish","Software Engineer", Admin);
+ad.createTask();
+ad.login();
